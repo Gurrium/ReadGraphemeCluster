62945b9c353f6153ad02723c134e92782732bf5f
gurrium <gurrium@hatena.ne.jp>
2023-03-05 23:05:57 +0900

    UnicodeScalarを読めるようにした

--- Sources/ReadGraphemeCluster/ReadGraphemeCluster.swift
+++ Sources/ReadGraphemeCluster/ReadGraphemeCluster.swift
@@ -25,6 +25,64 @@ enum GraphemeClusterBreak {
 
 public struct ReadGraphemeCluster {}
 
-public func readGraphemeCluster(from fileHandle: FileHandle) -> UnicodeScalar {
-    .init(0x0020)
+public struct AsyncCharacterSequence: AsyncSequence {
+    public typealias Element = Character
+
+    let fileHandle: FileHandle
+
+    public func makeAsyncIterator() -> AsyncIterator {
+        AsyncIterator(fileHandle: fileHandle)
+    }
+
+    public struct AsyncIterator: AsyncIteratorProtocol {
+        let fileHandle: FileHandle
+
+        public mutating func next() async throws -> Character? {
+            for try await unicodeScalar in fileHandle.bytes.unicodeScalars {
+                // TODO: detect grapheme cluster boundary
+                return Character(unicodeScalar)
+            }
+        }
+    }
+}
+
+// TODO: 必要なくなったので消す
+func read(_ fileHandle: FileHandle) async -> UnicodeScalar {
+    await withCheckedContinuation { continuation in
+        // TODO: endian
+        // TODO: UTF-16, UTF-32
+
+        let replacementCharacter = UnicodeScalar(0xfffd)!
+        var scalarValue: UInt32 = replacementCharacter.value
+        var container: UInt32 = 0
+        read(fileHandle.fileDescriptor, &container, 1)
+
+        func buildScalar(countOfRemainingBytes: Int) {
+            for _ in 0..<countOfRemainingBytes {
+                container <<= 4
+
+                let timer = Timer(timeInterval: 0 /* 0? */, repeats: false) { _ in
+                    continuation.resume(returning: replacementCharacter)
+                }
+
+                read(fileHandle.fileDescriptor, &container, 1)
+                timer.invalidate()
+            }
+
+            scalarValue = container
+        }
+
+        // ref: https://www.unicode.org/versions/Unicode15.0.0/ch03.pdf Table 3-6
+        if container & 0b1000_0000 >> 7 == 0b0001 { // 00000000 0xxxxxxx
+            buildScalar(countOfRemainingBytes: 0)
+        } else if container & 0b1110_0000 >> 5 == 0b0110 { // 00000yyy yyxxxxxx
+            buildScalar(countOfRemainingBytes: 1)
+        } else if container & 0b1111_0000 >> 4 == 0b1110 { // zzzzyyyy yyxxxxxx
+            buildScalar(countOfRemainingBytes: 2)
+        } else if container & 0b1111_1000 >> 3 == 0b0001_1110 { // 000uuuuu zzzzyyyy yyxxxxxx
+            buildScalar(countOfRemainingBytes: 3)
+        }
+
+        continuation.resume(returning: UnicodeScalar(scalarValue) ?? replacementCharacter)
+    }
 }
--- Tests/ReadGraphemeClusterTests/ReadGraphemeClusterTests.swift
+++ Tests/ReadGraphemeClusterTests/ReadGraphemeClusterTests.swift
@@ -1,12 +1,19 @@
 import XCTest
+import System
 @testable import ReadGraphemeCluster
 
 final class ReadGraphemeClusterTests: XCTestCase {
-    func test_readGraphemeCluster() throws {
-        let url = Bundle.module.url(forResource: "input", withExtension: "txt")!
-        let mockFileHandle = try! FileHandle(forReadingFrom: url)
-        let char = readGraphemeCluster(from: mockFileHandle)
+    func test_readGraphemeCluster() async throws {
+//        let url = Bundle.module.url(forResource: "input", withExtension: "txt")!
+//        let mockFileHandle = try! FileHandle(forReadingFrom: url)
+        let duplicatedStdin = try FileDescriptor.standardInput.duplicate()
 
-        XCTAssertEqual(char, " ")
+        for try await scalar in AsyncCharacterSequence(fileHandle: FileHandle(fileDescriptor: duplicatedStdin.rawValue)) {
+            print(scalar)
+        }
+
+        try duplicatedStdin.writeAll([
+
+        ])
     }
 }
